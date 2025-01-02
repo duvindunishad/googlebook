f8aaa53b9bb3c6118bf31fd50763e3c073cb5e10
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2025-01-02 18:34:08 +0530

    update routers

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
-import './components/style.css'
+import './components/style.css';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <>
-    <Header/>
-    <Main/>
-    <Footer/>
-    </>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<Main />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
