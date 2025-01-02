91d282d736fd098b2424a3788245fa98fb7c7b91
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2025-01-02 15:52:24 +0530

    create the google api to get book data by searching

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
+import axios from 'axios';
 
 const Main = () => {
+
+    const [search,setSearch] = useState('');
+    const [bookData,setBookData] = useState([]);
+    const searchBook = (e) => {
+        if(e.key === 'Enter'){
+            axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=AIzaSyADEtEvsddJThNHElf2RkUcvBCKjH6RBps`)
+            .then(res => setBookData(res.data.items))
+            .catch(err => console.log(err));
+        }
+    } 
   return (
         <>
       <div className='header'>
@@ -11,22 +22,15 @@ const Main = () => {
         <div className='row2'>
             <h2>Find your book</h2>
             <div className='search'>
-                <input type='text' placeholder='Search for books'/>
-                <button><i class="fas fa-search"></i></button>
+                <input type='text' placeholder='Search for books'
+                value={search} onChange={e=>setSearch(e.target.value)}
+                onKeyPress={searchBook} />
+                <button onClick={searchBook}><i className="fas fa-search">search</i></button>
             </div>
             <img src='./images/bg2.png' alt=''/>
         </div>
       </div>
       <div className='container'>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
-        <Card/>
         <Card/>
       </div>
     </>
