2fe7fed825627b09e76dac9e1d408814afa5c22c
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2025-01-02 17:35:13 +0530

    add header(nav bar)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
+import Header from './components/Header';
 import Main from './components/Main';
 import './components/style.css'
 
 function App() {
   return (
     <>
+    <Header/>
     <Main/>
     </>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+    return (
+        <header className="app-header">
+            <div className="header-content">
+                <h1>My React App</h1>
+                <nav>
+                    <ul className="nav-links">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
