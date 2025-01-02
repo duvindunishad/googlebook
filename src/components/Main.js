a25407e32b8ae3d7962c71e9cbec397587ba6429
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2025-01-02 13:29:19 +0530

    update the search bar

---
+++ src/components/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+        <>
+      <div className='header'>
+        <div className='row1'>
+            <h1>A room without book is like <br/> a body wit out a sole</h1>
+        </div>
+        <div className='row2'>
+            <h2>Find your book</h2>
+            <div className='search'>
+                <input type='text' placeholder='Search for books'/>
+                <button><i class="fa-solid fa-magnifying-glass"></i>Search</button>
+            </div>
+        </div>
+      </div>
+
+    </>
+  );
+}
+
+export default Main;
